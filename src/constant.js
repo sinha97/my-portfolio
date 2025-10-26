7f3575ebe98aeb2b2f1d5616328a9626a8f0456c
sinha97 <vk9633698@gmail.com>
2025-10-26 17:28:24 +0530

    add experience

--- src/constant.js
+++ src/constant.js
@@ -36,9 +36,9 @@ export const highlightProjects = [
 export const navItems = [
   { id: "home", label: "Home" },
   { id: "about", label: "About" },
+  { id: "experience", label: "Experience" },
   { id: "skills", label: "Skills" },
   { id: "projects", label: "Projects" },
-  // { id: "github", label: "GitHub" },
   { id: "contact", label: "Contact" },
 ];
 
